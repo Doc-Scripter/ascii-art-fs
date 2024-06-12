a5bab9a49a0a3054d9c37baf5e29278e00e35058
wnjuguna <wm453856@gmail>
2024-06-12 10:34:59 +0300

    Update: comdition to check what the third argument is

--- main.go
+++ main.go
@@ -9,15 +9,15 @@ import (
 
 func main() {
 	args := len(os.Args)
-	if args < 2 || args > 3{
+	if args < 2 || args > 3 {
 		fmt.Println("Usage: go run . [STRING] [BANNER]\nEX: go run . something standard")
 		return
 	}
 
-	//if only the string to be printed is provided
+	// if only the string to be printed is provided
 	if args == 2 {
 		s, err := os.ReadFile("Resources/standard.txt")
-		if err != nil {			
+		if err != nil {
 			fmt.Println("File not found")
 			return
 		}
@@ -25,10 +25,14 @@ func main() {
 		res := asciiArt.Tab(os.Args[1])
 		asciiArt.Paragraph(res, m)
 	}
-	
-	//if the string to be printed is provided and also the bannerfile
+
+	// if the string to be printed is provided and also the bannerfile
 	if args == 3 {
 		file := os.Args[2]
+		if !(file == "standard" || file == "thinkertoy" || file == "shadow" || file == "ac") {
+			fmt.Println("Usage: go run . [STRING] [BANNER]\nEX: go run . something standard")
+			return
+		}
 		switch file {
 		case "standard":
 			file = "Resources/standard.txt"
@@ -36,21 +40,21 @@ func main() {
 			file = "Resources/thinkertoy.txt"
 		case "shadow":
 			file = "Resources/shadow.txt"
+		case "ac":
+			file = "Resources/ac.txt"
 		default:
 			file = "Resources/standard.txt"
 		}
 
-		s, err := os.ReadFile(file)		
-		if err != nil {			
+		s, err := os.ReadFile(file)
+		if err != nil {
 			fmt.Println("File not found")
 			return
 		}
+
 		m := asciiArt.AsciiArt(string(s))
 		res := asciiArt.Tab(os.Args[1])
 		asciiArt.Paragraph(res, m)
 
-
 	}
-	
 }
-
