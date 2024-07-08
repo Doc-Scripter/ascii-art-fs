4ebb198f5d9a80d442b24c5ba5fb31ab4d283ab7
cliffootieno <clifford242526@gmail.com>
2024-07-08 18:11:38 +0300

    fixed problem of writting thinkertoy banner file incorrectly

--- functions/checkassets.go
+++ functions/checkassets.go
@@ -5,41 +5,71 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
-// compares local banner files to the one in cloud
+// Checkfiles compares local banner files to the ones in the cloud and updates them if necessary.
 func Checkfiles(s string) error {
 	remoteURL := ""
 	switch s {
 	case "resources/standard.txt":
 		remoteURL = "https://learn.zone01kisumu.ke/git/root/public/raw/branch/master/subjects/ascii-art/standard.txt"
-
 	case "resources/thinkertoy.txt":
-		remoteURL = "https://learn.zone01kisumu.ke/git/aadero/ascii-art-fs/raw/branch/wnjuguna/resources/thinkertoy.txt"
+		remoteURL = "https://learn.zone01kisumu.ke/git/root/public/raw/branch/master/subjects/ascii-art/thinkertoy.txt"
 	case "resources/shadow.txt":
 		remoteURL = "https://learn.zone01kisumu.ke/git/root/public/raw/branch/master/subjects/ascii-art/shadow.txt"
 	default:
 		remoteURL = "https://learn.zone01kisumu.ke/git/root/public/raw/branch/master/subjects/ascii-art/standard.txt"
-
 	}
-	// fetch the content of the remote file
-	remoteFile, err := http.Get(remoteURL)
+
+	// Fetch the content of the remote file
+	resp, err := http.Get(remoteURL)
 	if err != nil {
-		fmt.Println("Error fetching remote file:=", err)
+		fmt.Println("Error fetching remote file:", err)
 		return err
 	}
-	defer remoteFile.Body.Close()
+	defer resp.Body.Close()
 
-	str, err := os.Create(s)
-	if err != nil {
-		return err
-	}
-	defer str.Close()
-
-	_, err = io.Copy(str, remoteFile.Body)
-	if err != nil {
-		return err
+	// Check if the request was successful
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error: Received non-200 response code %d for URL %s\n", resp.StatusCode, remoteURL)
+		return fmt.Errorf("received non-200 response code %d for URL %s", resp.StatusCode, remoteURL)
 	}
 
+	// Read the remote file content into a string
+	remoteContent, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading remote file content:", err)
+		return err
+	}
+
+	// Normalize line endings from Windows-style (\r\n) to Unix-style (\n)
+	normalizedContent := strings.ReplaceAll(string(remoteContent), "\r\n", "\n")
+
+	// Remove the existing file if it exists
+	if _, err := os.Stat(s); err == nil {
+		err = os.Remove(s)
+		if err != nil {
+			fmt.Println("Error removing existing file:", err)
+			return err
+		}
+	}
+
+	// Create the new file
+	localFile, err := os.Create(s)
+	if err != nil {
+		fmt.Println("Error creating new file:", err)
+		return err
+	}
+	defer localFile.Close()
+
+	// Write the normalized content to the new local file
+	_, err = io.WriteString(localFile, normalizedContent)
+	if err != nil {
+		fmt.Println("Error writing content to new file:", err)
+		return err
+	}
+
+	fmt.Printf("Successfully updated %s.Please try again\n", s)
 	return nil
 }
