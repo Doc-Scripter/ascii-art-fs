a3a45051d38562a52114985f4b4c9a60f3139219
cliffootieno <clifford242526@gmail.com>
2024-07-08 14:45:20 +0300

    added capability to redownload bannerfiles

--- functions/checkassets.go
+++ functions/checkassets.go
@@ -1,9 +1,8 @@
 package ascii
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -20,7 +19,7 @@ func Checkfiles(s string) error {
 	case "resources/shadow.txt":
 		remoteURL = "https://learn.zone01kisumu.ke/git/root/public/raw/branch/master/subjects/ascii-art/shadow.txt"
 	default:
-		remoteURL = "https://learn.zone01kisumu.ke/git/root/public/raw/branch/master/subjects/ascii-art/standard.txt"
+		remoteURL = "https://learn.zone01kisumu.ke/git/root/public/raw/branch/master/subjects/ascii-art/shadow.txt"
 
 	}
 	// fetch the content of the remote file
@@ -31,37 +30,14 @@ func Checkfiles(s string) error {
 	}
 	defer remoteFile.Body.Close()
 
-	// create a new scanner to read from the remote file
-	remoteScanner := bufio.NewScanner(remoteFile.Body)
-
-	// read the content of the local file into a buffer
-	var localBuffer bytes.Buffer
-	file, err := os.ReadFile(s)
+	// create the file
+	str, err := os.Create(s)
 	if err != nil {
-		fmt.Println("Error reading ASCII art file:", err)
-		os.Exit(0)
+		return err
 	}
+	defer str.Close()
 
-	localBuffer.WriteString(string(file))
-
-	// create a scanner to read from the local buffer
-	localScanner := bufio.NewScanner(&localBuffer)
-
-	// compare the content of the remote file with the content of the local buffer
-	for remoteScanner.Scan() {
-		if !localScanner.Scan() || remoteScanner.Text() != localScanner.Text() {
-			fmt.Println("Files do not match: Remote file content is different from the local content")
-			var Stop string
-			fmt.Println("Do you still want to continue:Y or N")
-			fmt.Scan(&Stop)
-			if Stop == "N" {
-				os.Exit(0)
-			} else if Stop == "Y" {
-				return nil
-			}
-
-		}
-	}
-
-	return nil
+	// write the body to file
+	_, err = io.Copy(str, remoteFile.Body)
+	return err
 }
