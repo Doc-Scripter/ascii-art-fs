1f8b724160293b3adbbf21fddef67c2017d7f2fc
cliffootieno <clifford242526@gmail.com>
2024-07-08 15:19:06 +0300

    reverted to the correct code

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 		fmt.Println("Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard")
 		return
 	}
-	
+
 	standardCheckSum := "e194f1033442617ab8a78e1ca63a2061f5cc07a3f05ac226ed32eb9dfd22a6bf"
 	shadowCheckSum := "26b94d0b134b77e9fd23e0360bfd81740f80fb7f6541d1d8c5d85e73ee550f73"
 	thinkertoyCheckSum := "092d0cde973bfbb02522f18e00e8612e269f53bac358bb06f060a44abd0dbc52"
@@ -35,10 +35,10 @@ func main() {
 			log.Fatal(err)
 		}
 		checkSum := string(fmt.Sprintf("%x", h.Sum(nil)))
-if checkSum != standardCheckSum{
-	fmt.Println("The file has been modified. Please download the latest version from github.")
-	return
-}
+		if checkSum != standardCheckSum {
+			fmt.Println("The file has been modified. Please download the latest version from github.")
+			return
+		}
 
 		s, err := os.ReadFile("resources/standard.txt")
 		if err != nil {
@@ -90,10 +90,10 @@ if checkSum != standardCheckSum{
 		checkSum := string(fmt.Sprintf("%x", h.Sum(nil)))
 		if checkSum != standardCheckSum && checkSum != thinkertoyCheckSum && checkSum != shadowCheckSum {
 			fmt.Println("File contents have been corrupted. Redownload the banner files in the resources folder")
+			// asciiArt.Checkfiles(file)
 			return
 		}
 
-		asciiArt.Checkfiles(file)
 		s, err := os.ReadFile(file)
 		if err != nil {
 			fmt.Println("File not found")
