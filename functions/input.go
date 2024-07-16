23c222c9569e3389e642fad596f7a2a769f261d6
cliffootieno <clifford242526@gmail.com>
2024-07-16 16:04:00 +0300

    feat:added checksum check for ac file

--- functions/input.go
+++ functions/input.go
@@ -24,7 +24,7 @@ func Input(input []string) {
 	standardCheckSum := "e194f1033442617ab8a78e1ca63a2061f5cc07a3f05ac226ed32eb9dfd22a6bf"
 	shadowCheckSum := "26b94d0b134b77e9fd23e0360bfd81740f80fb7f6541d1d8c5d85e73ee550f73"
 	thinkertoyCheckSum := "092d0cde973bfbb02522f18e00e8612e269f53bac358bb06f060a44abd0dbc52"
-
+	acCheckSum := "cb217d33c89b7320ebd39f18573ab7ab90c3cb9042a7702f5dd21833524e73b2"
 	
 	if args == 2 {
 		banner, err := os.Open("resources/standard.txt")
@@ -63,7 +63,7 @@ func Input(input []string) {
 	if args == 3 {
 		file := os.Args[2]
 
-		banners := []string{"standard", "thinkertoy", "shadow"}
+		banners := []string{"standard", "thinkertoy", "shadow","ac"}
 		for i := range banners {
 			if file != banners[i] && i == len(banners)-1 {
 				fmt.Println("Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard")
@@ -98,7 +98,7 @@ func Input(input []string) {
 		}
 		checkSum := string(fmt.Sprintf("%x", bannerTemp.Sum(nil)))
 
-		if checkSum != standardCheckSum && checkSum != thinkertoyCheckSum && checkSum != shadowCheckSum {
+		if checkSum != standardCheckSum && checkSum != thinkertoyCheckSum && checkSum != shadowCheckSum &&checkSum!=acCheckSum {
 			fmt.Println("File contents have been corrupted. Redownloading the banner file")
 			Checkfiles(file)
 			return
